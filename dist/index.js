65db749a287e57f6f25de426c163b5cfef1181f4
Emediong Udoh <emediong5135@gmail.com>
2024-10-29 01:29:13 +0100

    setup middlewares

--- dist/index.js
+++ dist/index.js
@@ -1,20 +1,43 @@
-'use strict';
-var __importDefault =
-    (this && this.__importDefault) ||
-    function (mod) {
-        return mod && mod.__esModule ? mod : { default: mod };
-    };
-Object.defineProperty(exports, '__esModule', { value: true });
-const express_1 = __importDefault(require('express'));
-const dotenv_1 = __importDefault(require('dotenv'));
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const morgan_1 = __importDefault(require("morgan"));
+const helmet_1 = __importDefault(require("helmet"));
+const express_mongo_sanitize_1 = __importDefault(require("express-mongo-sanitize"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const compression_1 = __importDefault(require("compression"));
+const cors_1 = __importDefault(require("cors"));
 // Create express app
 const app = (0, express_1.default)();
 // Load environment variables
 dotenv_1.default.config();
 const PORT = process.env.PORT || 3000;
+// HTTP request logger middleware
+if (process.env.NODE_ENV !== 'production') {
+    app.use((0, morgan_1.default)('dev'));
+}
+// Secure express apps with various HTTP headers
+app.use((0, helmet_1.default)());
+// Parse JSON request body
+app.use(express_1.default.json());
+// Parse JSON request url
+app.use(express_1.default.urlencoded({ extended: true }));
+// Sanitize user-supplied data to prevent MongoDB operator injection
+app.use((0, express_mongo_sanitize_1.default)());
+// Enable cookie parser
+app.use((0, cookie_parser_1.default)());
+// Node.js compression middleware
+app.use((0, compression_1.default)());
+// Setup CORS
+app.use((0, cors_1.default)());
 // Test task route
-app.get('/', (req, res) => {
-    res.send('Testing SkyVentures Task API');
+app.post('/', (req, res) => {
+    // res.send('Testing SkyVentures Task API');
+    res.send(req.body);
 });
 // Start the dev server
 app.listen(PORT, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import helmet from 'helmet';
+import mongoSanitize from 'express-mongo-sanitize';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
 
 // Create express app
 const app = express();
@@ -9,9 +15,36 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+// HTTP request logger middleware
+if (process.env.NODE_ENV !== 'production') {
+    app.use(morgan('dev'));
+}
+
+// Secure express apps with various HTTP headers
+app.use(helmet());
+
+// Parse JSON request body
+app.use(express.json());
+
+// Parse JSON request url
+app.use(express.urlencoded({ extended: true }));
+
+// Sanitize user-supplied data to prevent MongoDB operator injection
+app.use(mongoSanitize());
+
+// Enable cookie parser
+app.use(cookieParser());
+
+// Node.js compression middleware
+app.use(compression());
+
+// Setup CORS
+app.use(cors());
+
 // Test task route
-app.get('/', (req: Request, res: Response) => {
-    res.send('Testing SkyVentures Task API');
+app.post('/', (req: Request, res: Response) => {
+    // res.send('Testing SkyVentures Task API');
+    res.send(req.body);
 });
 
 // Start the dev server
